3f5f32718d5dc127610bae7e45d583ded85cb8cd
dahnlund <david.ahnlund@gmail.com>
2023-09-24 14:20:27 +0200

    updated part 1

--- part1.m
+++ part1.m
@@ -1,5 +1,5 @@
 %%% Part 1 of CE2. David Ahnlund and Emil Gestsson
-
+clear, clc;
 %Coefficients
 a = 0.1; b = 0.4; Q0 = 7000; alpha0 = 50; Tout = 25; T0 = 100;
 
@@ -20,4 +20,17 @@ A = diag(D1, -1) + diag(D2, 0) + diag(D3, 1);
 Q_func = @(z) Q0 * sin((z-a)*pi / (b-a)) .* (a<=z).*(z<=b);
 Q = Q_func(z)';
 Q(1) = Q(1) - (-1/h^2 - v/(2*h))*T0;  %Boundary condition at T(0)
+
+% For the boundary T(1):
+%--------------------------------|
+alpha = @(v) sqrt(v^2/4 + alpha0^2) - v/2;
+
+theta = (v/(2*h) - 1/h^2) / (3/(2*h)+alpha(v));
+
+A(end,end-1) = (-1/h^2 - v/(2*h)-theta/(2*h));
+A(end,end) = (2/h^2 + 4/(2*h)*theta);
+Q(end) = Q(end) - theta * alpha(v)*Tout;
+%--------------------------------|
+
 T = A\Q;
+plot(z,T)
