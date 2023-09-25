e19dbdd1cb7972592e48dd766fdb46561c34a1ff
dahnlund <david.ahnlund@gmail.com>
2023-09-25 20:52:48 +0200

    updated part 2

--- part2.m
+++ part2.m
@@ -5,9 +5,49 @@ Lx = 12; Ly = 5; T_ext = 25;
 
 %% A
 
-N = 60*3; M = 25*3;
+N = 60; M = 25;
 h = Lx/N;  %should be same as Ly/M
 F = 2 * ones(N-1,M-1);
+
+Sx = 1/h^2 * (diag(-ones(N-2,1),-1)+diag(2*ones(N-1,1),0) + diag(-ones(N-2,1),1));
+Sy = 1/h^2 * (diag(-ones(M-2,1),-1)+diag(2*ones(M-1,1),0) + diag(-ones(M-2,1),1));
+
+%Boundary condition for x
+Sx(1,1) = 2/(3*h^2); Sx(1,2) = -2/(3*h^2);
+Sx(end,end) = 2/(3*h^2); Sx(end, end-1) = -2/(3*h^2);
+
+%Boundary condition for y
+Sy(end,end) = 2/(3*h^2); Sy(end, end-1) = -2/(3*h^2);
+
+A = kron(eye(size(Sy)),Sx) + kron(Sy, eye(size(Sx)));
+
+F(:,1) = F(:,1) + T_ext/h^2;
+
+f = reshape(F, (N-1)*(M-1),1);
+
+t = A\f;
+
+T = reshape(t, (N-1), (M-1));
+T0 = T_ext * ones(N-1,1);
+T = [T0 T];
+
+mesh(T)
+xlabel("y")
+ylabel("x")
+
+
+%% d
+
+N = 60*2;
+h = Lx/N;
+M = Ly/h;
+
+F_func = @(x,y) 100*exp(-1/2 * (x-4).^2 - 4*(y-1).^2);
+
+x = h:h:Lx-h;
+y = h:h:Ly-h;
+
+F = F_func(x',y);
 
 Sx = 1/h^2 * (diag(-ones(N-2,1),-1)+diag(2*ones(N-1,1),0) + diag(-ones(N-2,1),1));
 Sy = 1/h^2 * (diag(-ones(M-2,1),-1)+diag(2*ones(M-1,1),0) + diag(-ones(M-2,1),1));
