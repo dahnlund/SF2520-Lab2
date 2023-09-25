ec4a0ee62df5e0593eb49b01923012ac0a0c6dcf
dahnlund <david.ahnlund@gmail.com>
2023-09-25 16:34:29 +0200

    updated part2

--- part2.m
+++ part2.m
@@ -5,14 +5,28 @@ Lx = 12; Ly = 5; T_ext = 25;
 
 %% A
 
-N = 60; M = 25;
+N = 60*2; M = 25*2;
 h = Lx/N;  %should be same as Ly/M
-f = 2 * ones(N,M);
+F = 2 * ones(N-1,M-1);
 
 Sx = -1/h^2 * (diag(-ones(N-2,1),-1)+diag(2*ones(N-1,1),0) + diag(-ones(N-2,1),1));
 Sy = -1/h^2 * (diag(-ones(M-2,1),-1)+diag(2*ones(M-1,1),0) + diag(-ones(M-2,1),1));
 
+%Boundary condition for x
+Sx(1,1) = 2/(3*h^2); Sx(1,2) = -2/(3*h^2);
+Sx(end,end) = 2/(3*h^2); Sx(end, end-1) = 4/(3*h^2);
+
+%Boundary condition for y
+Sy(end,end) = 2/(3*h^2); Sy(end, end-1) = 4/(3*h^2);
+
 A = kron(eye(size(Sy)),Sx) + kron(Sy, eye(size(Sx)));
 
+F(:,1) = F(:,1) - T_ext;
 
+f = reshape(F, (N-1)*(M-1),1);
 
+u = A\f;
+
+U = reshape(u, (N-1), (M-1));
+
+mesh(U)
